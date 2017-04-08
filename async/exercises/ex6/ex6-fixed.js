972805363f52405967af0d951e3715b965c187a1
Kyle Hennessy <kyle@lennd.com>
2017-04-08 09:15:50 -0700

    git init

---
+++ async/exercises/ex6/ex6-fixed.js
@@ -0,0 +1,41 @@
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+function getFile(file) {
+	return ASQ(function(done){
+		fakeAjax(file,done);
+	});
+}
+
+
+ASQ()
+.seq(
+	...["file1","file2","file3"]
+	.map(function(file){
+		var sq = getFile(file);
+		return function(){
+			return sq.val(output);
+		};
+	})
+)
+.val(function(){
+	output("Complete!");
+});
